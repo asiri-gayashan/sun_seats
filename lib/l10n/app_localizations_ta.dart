bfcae36b136720b59e2e4810a3dfb82a4acea43e
asiri-gayashan <gayashanx@gmail.com>
2026-03-28 13:00:16 +0530

    feat: initialize project structure with multi-language localization support and location permissions

---
+++ lib/l10n/app_localizations_ta.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Tamil (`ta`).
+class AppLocalizationsTa extends AppLocalizations {
+  AppLocalizationsTa([String locale = 'ta']) : super(locale);
+
+  @override
+  String get appTitle => 'ShadeSeat LK';
+
+  @override
+  String get startLocation => 'தொடங்கும் இடம்';
+
+  @override
+  String get endLocation => 'முடிவிடம்';
+}
